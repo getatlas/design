d65e8109645871c5cc90e4bd77f9b1cdd4243fbf
Adam Rudnicki <contact@adamrudnicki.com>
2016-08-29 22:51:09 +0200

    First steps in gulp config Add variables

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.browserSync({
-        proxy: 'getatlas.dev'
+        proxy: 'getatlas.dev',
+        host: '192.168.0.12'
     });
 });
