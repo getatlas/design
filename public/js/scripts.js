714f89486c1319e2280234a029248bdd40ace58b
Adam Rudnicki <contact@adamrudnicki.com>
2016-09-02 23:31:16 +0200

    Add dropdown menu Add notification button WIP Search

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,8 @@
 var collapseSlider = function() {
   var onLoad = true;
-  document.querySelector('.js-toggle-sidebar').addEventListener('click', function () {
+  var toggleSidebarButton = document.querySelector('.js-toggle-sidebar');
+
+  toggleSidebarButton.addEventListener('click', function() {
      if (onLoad) {
        document.querySelector('.js-sidebar').classList.add('sidebar--loaded');
        onLoad = true;
@@ -10,6 +12,41 @@ var collapseSlider = function() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+var dropdownMenuToggle = function() {
+  var dropdownMenus = document.querySelectorAll('.js-dropdown-menu-nav');
+  var dropdownMenu = document.querySelector('.js-dropdown-menu');
+  var dropdownMenuTrigger = document.querySelector('.js-dropdown-menu-trigger');
+  var dropdownMenuNav = document.querySelector('.js-dropdown-menu-nav');
+
+  Array.prototype.forEach.call(dropdownMenus, function(el, i) {
+    el.style.maxHeight = el.offsetHeight + 'px';
+    console.log(el.getAttribute('data-dropdown-hidden'));
+
+    if (Boolean(el.getAttribute('data-dropdown-hidden')) === true) {
+      el.classList.add('dropdown-menu__children--hidden');
+    }
+  });
+
+  dropdownMenuTrigger.addEventListener('click', function() {
+    this.classList.add('active');
+    this.nextElementSibling.classList.contains('dropdown-menu__children--hidden') ? true : this.classList.remove('active');
+    this.nextElementSibling.classList.toggle('dropdown-menu__children--hidden');
+  });
+}
+
+var search = function() {
+  var searchField =  document.querySelector('.js-search-field');
+
+  searchField.addEventListener('focusin', function() {
+    searchField.parentNode.classList.add('search--active');
+  });
+  searchField.addEventListener('focusout', function() {
+    searchField.parentNode.classList.remove('search--active');
+  });
+}
+
+window.addEventListener('load', function () {
   collapseSlider();
+  dropdownMenuToggle();
+  search();
 });
