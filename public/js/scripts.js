a328f74e2b19499987134f76c359da487fb45f22
Adam Rudnicki <contact@adamrudnicki.com>
2016-09-12 22:51:45 +0200

    WIP Tabs, Fix toolbar z-index issue

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -49,33 +49,68 @@ var tabs = function() {
   var tabsNavs = document.querySelectorAll('.js-tabs-nav');
   var tabsNavItem = document.querySelector('.js-tabs-nav-item');
   var tabsNavItems = document.querySelectorAll('.js-tabs-nav-item');
+  var tabsContentItems = document.querySelectorAll('.js-tabs-content-item');
+  var tabsContentItem = document.querySelector('.js-tabs-content-item');
 
-  console.log(tabsNavs.length);
-
+  //Handle Tab Nav Switch
   for (var i = 0; i < tabsNavs.length; i++) {
-    var element = tabsNavs.parentNode.children[i].classList.add('dupa');
+    var element = tabsNavs[i];
+    var checker = true;
+    element.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      if (checker === false) return false;
+
+      var tabsContentPath;
+
+      if (e.target !== e.currentTarget) {
+        Array.prototype.forEach.call(this.querySelectorAll('.js-tabs-nav-item'), function(element, i) {
+          if (element.parentNode.classList.contains('tabs__nav__item--active') === true) {
+            element.parentNode.classList.remove('tabs__nav__item--active');
+
+            return false;
+          }
+        });
+
+        tabsContentPath = e.target.getAttribute('href');
+        e.target.parentNode.classList.add('tabs__nav__item--active');
+
+        Array.prototype.forEach.call(this.closest('.js-tabs').querySelectorAll('.js-tabs-content-item'), function(element, i) {
+          var tabPath = '#' + element.getAttribute('id');
+          if (tabPath == tabsContentPath) {
+            console.log('No Witamy ponownie Panie Junior Front Adamie');
+            element.parentNode.style.height = element.offsetHeight + 'px';
+
+            element.closest('.js-tabs').querySelector('.tabs__content__item--active').classList.add('tabs__content__item--hiding');
+            element.classList.add('tabs__content__item--active');
+
+            setTimeout(function() {
+              element.closest('.js-tabs').querySelector('.tabs__content__item--hiding').classList.remove('tabs__content__item--active');
+              element.closest('.js-tabs').querySelector('.tabs__content__item--hiding').classList.remove('tabs__content__item--hiding');
+            }, parseFloat(getComputedStyle(element.closest('.js-tabs').querySelector('.tabs__content__item--hiding')).transitionDuration) * 1000);
+
+            return false;
+          }
+        });
+
+        checker = false;
+
+        setTimeout(function() {
+          checker = true;
+          console.log('Uff!');
+        }, 1000);
+      }
+    });
   }
 
-  tabsNav.addEventListener('click', function(event) {
-    // this.classList.add('tabs__nav__item--active')
-    if (event.target !== event.currentTarget) {
-      Array.prototype.forEach.call(tabsNavItems, function(element, i) {
-        if (element.parentNode.classList.contains('tabs__nav__item--active') === true) {
-          element.parentNode.classList.remove('tabs__nav__item--active');
-        }
-      });
-      event.target.parentNode.classList.add('tabs__nav__item--active');
-    }
-    event.preventDefault();
-    event.stopPropagation();
-  });
+  //Set maxHeight for first active element
+  Array.prototype.forEach.call(tabsContentItems, function(element, i) {
+    if (element.classList.contains('tabs__content__item--active') === true) {
+      element.parentNode.style.height = element.offsetHeight + 'px';
 
-  // var tabsContent = document.querySelector('.js-tabs-content');
-  // var tabsContentItem = document.querySelector('.js-tabs-content-item');
-  //
-  // tabsNavItem.addEventListener('click', function() {
-  //     console.log('click handler fired');
-  // });
+      return false;
+    }
+  });
 }
 
 window.addEventListener('load', function () {
