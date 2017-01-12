3f2ceda537bbce2d25c8037bfaa9fadce0b0747e
Adam Rudnicki <adam@iMac-Adam.local>
2017-01-12 22:13:39 +0100

    Create gulp file for sass and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create('server');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
 
-elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.browserSync({
-        proxy: 'getatlas.dev',
-        host: '192.168.0.12'
+gulp.task('styles', function() {
+    return gulp.src('./resources/assets/sass/app.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['styles'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './public',           
+        },
     });
+
+    gulp.watch('./resources/**/*.scss', ['styles']);
+    gulp.watch('./public/*.html').on('change', browserSync.reload);
 });
